fd39e457adb1fc03c3df4751929c98d904024e54
Daren Nkomo <daren.nkomo@gmail.com>
2019-06-03 18:52:05 -0600

    added button classes

--- Calculator/Classes/Buttons/Button.cs
+++ Calculator/Classes/Buttons/Button.cs
@@ -1,18 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Calculator.Classes.Buttons
+﻿namespace Calculator.Classes.Buttons
 {
+    using Calculator.Classes.Operators;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
     public class Button
     {
         int xCoordinate;
         int yCoordinate;
+        Operator currentOperator;
 
         public int YCoordinate { get => yCoordinate; set => yCoordinate = value; }
         public int XCoordinate { get => xCoordinate; set => xCoordinate = value; }
+        public Operator CurrentOperator { get => currentOperator; set => currentOperator = value; }
 
         public void Click (object sender, EventArgs e)
         {
---
+++ Calculator/Classes/Buttons/ButtonZero.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Classes.Buttons
+{
+    class ButtonZero : Button, IButtonNumber
+    {
+        int buttonValue;
+
+        public int ButtonValue { get => buttonValue; set => buttonValue = ButtonValue; }
+    }
+}
